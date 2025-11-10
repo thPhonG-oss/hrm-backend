4b2e586b1831ef09da83d99a9e98df316dfeffee
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-11-11 01:06:07 +0700

    config enviroment variables

--- src/main/java/com/phong/hrm_backend/configuration/SecurityConfig.java
+++ src/main/java/com/phong/hrm_backend/configuration/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://hrm-frontend-phi.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", System.getenv("FRONTEND_URL")));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("*"));
